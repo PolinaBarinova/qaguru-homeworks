0d36c3916f7dd9bd70ebb13aa34c9eafed682f3b
Polina Barinova <barinova_polina@list.ru>
2025-04-19 20:11:18 +0300

    сhecking the sample code for JUnit5

---
+++ src/test/java/SelenideWikiTest.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideWikiTest {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+
+    void checkCodeForJUnit5() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $("#wiki-body").$(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
