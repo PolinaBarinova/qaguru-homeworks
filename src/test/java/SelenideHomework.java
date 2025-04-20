b5e3eed01194d408eb032c1fa7558516f4abd07f
Polina Barinova <barinova_polina@list.ru>
2025-04-20 17:59:27 +0300

    added tests from homework 5

---
+++ src/test/java/SelenideHomework.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideHomework {
+
+    @Test
+
+    void openPageEnterprize () {
+        open("https://github.com");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $$("a.HeaderMenu-dropdown-link").findBy(text("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+    }
+
+    @Test
+
+    void actionsDragAndDropTest () {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+
+    void dragAndDropTest (){
+        // ошибка "incompatible types: SelenideElement cannot be converted to DragAndDropOptions"
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        // $("#column-a").dragAndDrop($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+}
+
